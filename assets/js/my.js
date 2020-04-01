8620b457222e3b1359b7a22af9d1b5b183f0550c
JunkJumper <38364274+JunkJumper@users.noreply.github.com>
2020-04-01 12:32:01 +0200

    initial commit

---
+++ assets/js/my.js
@@ -0,0 +1,23 @@
+function init() {
+    console.log("loading ok")
+    $(document).ready(function(){
+
+        $(".owlcarousel").owlcarousel();
+        console.log("carrousel load ok !")
+    });
+
+    
+}
+
+$(document).ready(function() {
+
+    $("#carousel-index").owl-carousel({
+        itemsDesktop : [1499,4],
+        itemsDesktopSmall : [1199,3],
+        itemsTablet : [899,2],
+        itemsMobile : [599,1],
+        navigation : true,
+        navigationText : ['<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-left fa-stack-1x fa-inverse"></i></span>','<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-right fa-stack-1x fa-inverse"></i></span>'],
+    });
+    
+});
